6d8cdf58094ccf2709d7e83b829de10f3315c63f
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-04 18:41:06 -0500

    feat: added first response

--- src/TheMenace.java
+++ src/TheMenace.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class TheMenace {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, My Name is Onyx!");
+        System.out.println("Wanna play?");
+
+    }
+
+    public static boolean Onyx(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Onyx know's a few commands, try giving him one to see his reaction!");
+        String userInput = sc.nextLine();
+    
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput == "") {
+            System.out.println("Fine, be that way!");
+        } else {
+            System.out.println("Eh, whatever man.");
+        }
     }
 }
