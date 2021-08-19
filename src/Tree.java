a22d7520f78aaa8baf068792aaa8c8e3e1f3e872
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-18 23:44:16 -0500

    feat: update

---
+++ src/Tree.java
@@ -0,0 +1,16 @@
+public class Tree {
+    public static void main(String[] args){
+        // Lets build that tree
+
+        System.out.println("Hi! I'm Jonathan.");
+        System.out.println("Today we are going to plant a tree!");
+        System.out.println("      *      ");
+        System.out.println("    *****    ");
+        System.out.println("   *******   ");
+        System.out.println("  *********  ");
+        System.out.println(" *********** ");
+        System.out.println("     **      ");
+        System.out.println("");
+        System.out.println("");
+    }
+}
