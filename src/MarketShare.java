63bea1b9eb00ee98ae51654028f3c7703787d8a6
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-19 23:35:52 -0500

    feat: added MarketShare.java

---
+++ src/MarketShare.java
@@ -0,0 +1,9 @@
+public class MarketShare {
+    public static void main(String[] args){
+            
+    	double androidShare = 81.7;
+        
+        System.out.println(androidShare + " is controlled by android 18.");
+
+    }
+}
