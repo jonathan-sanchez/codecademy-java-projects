1a50ed2dbdc33ccb161ca84a7bacd3442167c1db
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-23 13:47:29 -0500

    feat: added Modulo.java

---
+++ src/Modulo.java
@@ -0,0 +1,9 @@
+public class Modulo {
+    public static void main(String[] args) {
+
+        int students = 26;
+        int leftOut = students % 3;
+
+        System.out.println(leftOut);
+    }
+}
