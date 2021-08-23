47606c7c103b0c494214b4951df91bda1caa0f3d
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-22 21:32:12 -0500

    feat: added MyProfile.java

---
+++ src/MyProfile.java
@@ -0,0 +1,14 @@
+public class MyProfile {
+    public static void main(String[] args) {
+
+        String name = "Jonathan";
+
+        int age = 28;
+
+        double desiredSalary = 90000;
+
+        char gender = 'M';
+
+        boolean lookingForJob = true;
+    }
+}
