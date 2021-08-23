76e69bcba5f2b824580c1b278cb968b6f3063176
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-23 14:05:56 -0500

    feat: added Operations.java

---
+++ src/BakeSale.java
@@ -0,0 +1,12 @@
+public class BakeSale {
+    public static void main(String[] args) {
+
+        int numCookies = 17;
+
+        numCookies -= 3;
+
+        numCookies /= 2;
+
+        System.out.println(numCookies);
+    }
+}
