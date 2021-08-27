8a1de79182de016c9d481eaf41b2ec79698e638c
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-27 18:07:25 -0500

    feat: updates

--- src/MathMagic.java
+++ src/MathMagic.java
@@ -2,14 +2,19 @@ public class MathMagic {
     public static void main(String[] args) {
 
         //myNumber is the original number
-        int myNumber = 7;
+        int myNumber = 200;
 
         //Step 1 - Step 6
         int stepOne = myNumber * myNumber;
+
         int stepTwo = stepOne + myNumber;
+
         int stepThree = stepTwo / myNumber;
+
         int stepFour = stepThree + 17;
+
         int stepFive = stepFour - myNumber;
+
         int stepSix = stepFive / 6;
 
         //Print last step
