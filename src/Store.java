abf7114b11e85bb356583417e92cbf382135451d
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-27 18:36:33 -0500

    feat: added a third store

--- src/Store.java
+++ src/Store.java
@@ -16,9 +16,12 @@ public class Store {
     public static void main(String[] args) {
         Store lemonadeStand = new Store("lemonade", 42, .99);
         Store cookieShop = new Store("cookies", 12, 3.75);
+        Store vapeShop = new Store("vape juice", 50, 19.99);
 
-        System.out.println("Our first shop sells " + lemonadeStand.productType + " at" + lemonadeStand.inventoryPrice + " per unit.");
+        System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
 
-        System.out.println("Our second shop sells " + cookieShop.productType + " at" + cookieShop.inventoryPrice + " per unit.");
+        System.out.println("Our second shop sells " + cookieShop.productType + " at " + cookieShop.inventoryPrice + " per unit.");
+
+        System.out.println("Our third shop sells " + vapeShop.productType + " at " + vapeShop.inventoryPrice + " per unit.");
     }
 }
