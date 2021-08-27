735db09fd5482ff38aed573b9a3271f50f6b38a6
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-27 17:41:30 -0500

    feat: added Zoo.java and updated Song.java

--- src/Song.java
+++ src/Song.java
@@ -11,5 +11,7 @@ public class Song {
 
         System.out.println(line1.equals(line2));
         System.out.println(line2.equals(line3));
+
+        
     }
 }
---
+++ src/Zoo.java
@@ -0,0 +1,12 @@
+public class Zoo {
+    public static void main(String[] args){
+
+        int animals = 12;
+
+        String species = "Zebra";
+
+        String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+
+        System.out.println(zooDescription);
+    }
+}
