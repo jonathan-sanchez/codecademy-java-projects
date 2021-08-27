3ed322ec57b531c8ba3c737ba4ed5ddfa5b4a998
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-27 17:46:20 -0500

    feat: added Final.java

---
+++ src/Final.java
@@ -0,0 +1,11 @@
+public class Final {
+    public static void main(String[] args){
+
+        final double pi = 3.14;
+
+        System.out.println(pi);
+
+        //While trying to change the value of pi below you will get an error because pi cannot be changed
+        // double pi = "3.15";
+    }
+}
