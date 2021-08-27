c64d651337afa30ac3ac23fbc7ba7307ecb0ef0c
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-27 17:29:04 -0500

    feat: updated Song.java

--- src/Song.java
+++ src/Song.java
@@ -1,8 +1,15 @@
 public class Song {
     public static void main(String[] args) {
 
-        String openingLyrics = "Yesterday, all my troubles seemed so far away";
+        String openingLyrics = "Hey Jude, dont make it bad";
 
         System.out.println(openingLyrics);
+
+        String line1 = "Nah nah nah nah nah nah nah nah yeah";
+        String line2 = "Nah nah nah nah nah, nah nah nah, hey Jude";
+        String line3 = "Nah nah nah nah nah, nah nah nah, hey Jude";
+
+        System.out.println(line1.equals(line2));
+        System.out.println(line2.equals(line3));
     }
 }
