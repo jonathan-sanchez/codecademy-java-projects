356c166b1762ef2bd1c1ca7cc309f10946fd4111
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-08-27 18:27:50 -0500

    feat: added Store.java

---
+++ src/Store.java
@@ -0,0 +1,19 @@
+public class Store {
+
+    //Instance Fields
+    String productType;
+    int inventoryCount;
+    double inventoryPrice;
+
+    //Constructor Method
+    public Store(String product, int count, double price) {
+        productType = product;
+        inventoryCount = count;
+        inventoryPrice = price;
+    }
+    
+    //Main Method
+    public static void main(String[] args) {
+
+    }
+}
