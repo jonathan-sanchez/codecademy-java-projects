4ab7cb368c112e39fa54a652e3acebbf27124db9
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-09-06 23:32:22 -0500

    feat: added two constructors for Car.java

--- src/Car.java
+++ src/Car.java
@@ -2,9 +2,16 @@ public class Car {
     String color;
     int mpg;
     boolean isElectric;
-   
+    
+    //Constructor 1
     public Car(String carColor, int milesPerGallon) {
         color = carColor;
+        mpg = milesPerGallon;
+    }
+
+    //Constructor 2
+    public Car(boolean electricCar, int milesPerGallon) {
+        isElectric = electricCar;
         mpg = milesPerGallon;
     }
 }
