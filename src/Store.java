eec57dbbf28e1754c68cdfa9e01d075e8f80dfab
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-09-16 23:36:39 -0500

    feat: new shop

--- src/Store.java
+++ src/Store.java
@@ -25,6 +25,7 @@ public class Store {
         Store videoGameStore = new Store("video games", 100, 5.99);
         Store craftShop = new Store("jewelery", 50, 99.99);
         Store petShop = new Store("pet supplies", 100, 29.99);
+        Store shoeStore = new Store("shoes", 50, 49.99);
 
         //Print the instance below
         System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
