d0d320708455580170318a8c1e5583b5cb7a1965
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-09 17:10:34 -0600

    feat: added greeting to jonbot

---
+++ src/JonBot.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JonBot {
+    public static void main(String[] args) {
+        System.out.println("Hey Beautiful! This is the bubba bot!");
+        talkToJon();
+    }
+
+    public static void talkToJon() {
+        boolean keepTalking;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("As Jon walks in with his head held high looks at you and says 'Hey beautiful!'");
+    }
+}
--- src/Shopping.java
+++ src/Shopping.java
@@ -4,8 +4,11 @@ public class Shopping {
         String[] groceryItems = {"steak", "milk", "jelly beans"};
         double[] prices = {25.00, 2.95, 2.50};
 
-        // Adding ham to the groceries
+        // Adding ham to the groceries array
         groceryItems[3] = "ham";
         prices[3] = 4.99;
+
+        //^^does not work because an array in java has a fixed size, you cannot add or remove elements.
+        
     }
 }
