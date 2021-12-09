ffbb74beb3a35f17e923fb41fe30c5f485ce5cad
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-09 14:19:56 -0600

    added: shopping.java

---
+++ src/Shopping.java
@@ -0,0 +1,9 @@
+public class Shopping {
+    public static void main(String[] args){
+
+        String[] groceryItems = {"steak", "milk", "jelly beans"};
+        double[] prices = {25.00, 2.95, 2.50};
+
+        // Adding ham to the groceries
+    }
+}
