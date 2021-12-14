4595193e1e95689604dc4d93ddddfa7631b61699
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-14 15:21:25 -0600

    feat: refactored and added method for printing battery life of droid instances

--- src/Droid.java
+++ src/Droid.java
@@ -4,27 +4,36 @@ public class Droid {
     int batteryLevel;
     String name;
 
-    //Main method
-    public static void main(String[] args) {
-
-        //New instance of Droid named "Codey"
-        Droid myDroid = new Droid("Codey");
-        //Print the variable for myDroid
-        System.out.println(myDroid); //When printing it returns an object instead of the droid name. Must define a toString method for object
-    }
-
     //Constructor method for Droid
     public Droid(String droidName) { //This method has one parameter "String droidName"
         name = droidName;
         //Every new instance of Droid will have a battery level of 100
-        batteryLevel = 100;
+        batteryLevel = 100 - 10; //Reassigned the instance field to be the current value minus ten
     }
     //toString method within Droid
     public String toString() {
         return "Hello, I am the droid: " + name;
     }
     //Perform task method for Droid
-    public String performTask(String task) {
-        return name + " is performing task: " + task;
+    public void performTask(String task) {
+        batteryLevel = batteryLevel - 10;
+        System.out.println(name + " is performing task: " + task);
     }
+    //Method for printing battery life for droid instances
+    public void energyReport() {
+        System.out.println("Current battery level: " + batteryLevel + "%");
+    }
+
+    //Main method
+    public static void main(String[] args) {
+
+        //New instance of Droid named "Codey"
+        Droid codey = new Droid("Codey");
+        //Print the variable for myDroid
+        System.out.println(codey); //When printing it returns an object instead of the droid name. Must define a toString method for object
+        codey.performTask("dancing");
+        codey.performTask("coding");
+        codey.energyReport();
+    }
+
 }
