84894ce587a9b71273244fff5655c4ad75faa7ef
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-14 16:36:11 -0600

    created ToDoList.java

---
+++ src/ToDoList.java
@@ -0,0 +1,13 @@
+//import the ArrayList package here
+import java.util.ArrayList;
+
+public class ToDoList {
+
+    //Main method
+    public static void main(String[] args){
+
+        //Create toDoList below:
+        ArrayList<String> toDoList = new ArrayList<String>();
+        
+    }
+}
