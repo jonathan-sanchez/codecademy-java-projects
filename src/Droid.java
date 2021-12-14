ef05074bcafc82388eb2c57be63347158655923d
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-14 14:12:31 -0600

    feat: added constructor method for Droid class

--- src/Droid.java
+++ src/Droid.java
@@ -1,3 +1,19 @@
+//Class Droid
 public class Droid {
-    
+    //Instance field
+    int batteryLevel;
+    String name;
+
+    //Main method
+    public static void main(String[] args) {
+
+        //New instance of Droid named "Codey"
+        Droid myDroid = new Droid("Codey");
+    }
+
+    //Constructor method for Droid
+    public Droid(String droidName) { //This method has one parameter "String droidName"
+        name = droidName;
+        batteryLevel = 100;
+    }
 }
