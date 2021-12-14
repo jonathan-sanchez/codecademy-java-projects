3649b58ad1a90630da07740b27e179b11e612dbc
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-14 15:40:00 -0600

    created CarLoan.java

---
+++ src/CarLoan.java
@@ -0,0 +1,6 @@
+public class CarLoan {
+    //This program calculates your monthly payment after taking out a car loan.
+    public static void main(String[] args) {
+        //invoke instances here
+    }
+}
