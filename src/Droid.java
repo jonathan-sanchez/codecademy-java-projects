62d5bd2f1aae8190107927c08cb246120c4a96c9
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-14 14:23:01 -0600

    feat: added a toString method within droid class

--- src/Droid.java
+++ src/Droid.java
@@ -9,11 +9,18 @@ public class Droid {
 
         //New instance of Droid named "Codey"
         Droid myDroid = new Droid("Codey");
+        //Print the variable for myDroid
+        System.out.println(myDroid); //When printing it returns an object instead of the droid name. Must define a toString method for object
     }
 
     //Constructor method for Droid
     public Droid(String droidName) { //This method has one parameter "String droidName"
         name = droidName;
+        //Every new instance of Droid will have a battery level of 100
         batteryLevel = 100;
+    }
+    //toString method within Droid
+    public String toString() {
+        return "Hello, I am the droid: " + name;
     }
 }
