2134a331bca9650f17663dd49f0fb80767cbb452
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-12-14 16:01:52 -0600

    feat: updated if statement

--- src/CarLoan.java
+++ src/CarLoan.java
@@ -1,6 +1,29 @@
+//CarLoan class
 public class CarLoan {
     //This program calculates your monthly payment after taking out a car loan.
     public static void main(String[] args) {
         //invoke instances here
+        int carLoan = 25000;
+        int loanLength = 5;
+        int interestRate = 5;
+        int downPayment = 5000;
+
+        //if statement to calculate monthly payment.
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("Error! You must take out a valid car loan."); //Helpful error message for the user.
+        } else if (downPayment >= carLoan) {
+            System.out.println("The car can be paid in full."); //Helpful message for the user
+
+            //if none of the previous boolean expressions evaluate to true, calculate monthly payment.
+        } else {
+            int remainingBalance = carLoan - downPayment;
+            //Convert loan length from years to months.
+            int months = loanLength * 12;
+            //This represents the monthly payment without interest included
+            int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance * interestRate / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
+        }
     }
 }
