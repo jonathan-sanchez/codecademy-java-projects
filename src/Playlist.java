031bac685849bc5cc6c6028920538bcf2202664e
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-11 16:50:53 -0600

    feat: update

--- src/Playlist.java
+++ src/Playlist.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Playlist {
     
     public static void maain(String[] args) {
+        
+        //Creating a new ArrayList
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
 
     }
 }
