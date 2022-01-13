e62a471b114477b1e095f6e971b84a099e2803c1
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-13 14:59:56 -0600

    feat: added and removed songs from playlist

--- src/Playlist.java
+++ src/Playlist.java
@@ -7,5 +7,29 @@ public class Playlist {
         //Creating a new ArrayList
         ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
 
+        // adding songs to playlist
+        desertIslandPlaylist.add("War pigs - Black Sabbath");
+        desertIslandPlaylist.add("Holy Diver - Dio");
+        desertIslandPlaylist.add("Master Exploder - Tenacious D");
+        desertIslandPlaylist.add("Sadistic Magician - Municipal Waste");
+        desertIslandPlaylist.add("Enemy - Imagine Dragons");
+        desertIslandPlaylist.add("Antivist - Bring me the Horizon");
+        desertIslandPlaylist.add("I've just seen a face - The Beatles");
+        desertIslandPlaylist.add("In these streets - Zro");
+        desertIslandPlaylist.add("Wait - Maroon 5");
+        desertIslandPlaylist.add("Earth - Lil Dicky");
+        
+        // printing playlist
+        // System.out.println(desertIslandPlaylist);
+        
+        // checking playlist size
+        // System.out.println(desertIslandPlaylist.size());
+        
+        // removing songs
+        desertIslandPlaylist.remove("Sadistic Magician - Municipal Waste");
+        desertIslandPlaylist.remove("I've just seen a face - The Beatles");
+        desertIslandPlaylist.remove("Antivist - Bring me the Horizon");
+        desertIslandPlaylist.remove("Wait - Maroon 5");
+        desertIslandPlaylist.remove("Earth - Lil Dicky");
     }
 }
