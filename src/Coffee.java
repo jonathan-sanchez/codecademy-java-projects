6731b8ff57cff212c88924734cb1ef31524ddd43
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-20 17:10:15 -0600

    feat: completed for loop version

--- src/Coffee.java
+++ src/Coffee.java
@@ -2,12 +2,17 @@ public class Coffee {
     
     public static void main(String[] args) {
 
-        int cupsOfCoffee = 1;
+        // int cupsOfCoffee = 1;
+
         //while loop version
         // while(cupsOfCoffee <= 100) {
         //     System.out.println("Jon drinks cup of coffee #" + cupsOfCoffee);
         //     cupsOfCoffee++;
         // }
+        
         //for loop version
+        for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+            System.out.println("Jon drinks cup of coffee #" + cupsOfCoffee);
+        }
     }
 }
