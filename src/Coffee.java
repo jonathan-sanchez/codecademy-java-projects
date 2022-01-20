ee8dcdc441460ef273690a137f74a6825fa7d9fd
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-20 16:52:10 -0600

    added: Coffee class

---
+++ src/Coffee.java
@@ -0,0 +1,11 @@
+public class Coffee {
+    
+    public static void main(String[] args) {
+
+        int cupsOfCoffee = 1;
+
+        while(cupsOfCoffee <= 100) {
+            System.out.println("Jon drinks cup of coffee #" + cupsOfCoffee);
+        }
+    }
+}
