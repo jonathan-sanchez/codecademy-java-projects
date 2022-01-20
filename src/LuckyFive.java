88e7478cc2ed21e7bd736e186a10e1cbcf0c21c0
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-20 16:24:02 -0600

    added LuckyFive class

---
+++ src/LuckyFive.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class LuckyFive {
+    
+    public static void main(String[] args) {
+
+        Random randomGenerator = new Random();
+
+        int dieRoll =
+    }
+}
