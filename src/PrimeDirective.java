e5a130b9b6f443b0571ffeef80eb6873384aaa03
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-23 09:09:11 -0600

    added main method and numbers array

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -1,6 +1,11 @@
 public class PrimeDirective {
     
+
     public static void main(String[] args) {
-        
+
+        PrimeDirective pd = new PrimeDirective();
+
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
     }
 }
