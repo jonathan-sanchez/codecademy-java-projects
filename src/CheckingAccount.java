47decb6cf55b1bdae31fbaee687a04bfd083983e
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-01-24 17:16:34 -0600

    feat: added the CheckingAccount constructor for CheckingAccount class along with private instances

---
+++ src/CheckingAccount.java
@@ -0,0 +1,14 @@
+public class CheckingAccount {
+    
+    private String name;
+    private int balance;
+
+    public CheckingAccount(String inputName, int inputBalance) {
+        name = inputName;
+        balance = inputBalance;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
