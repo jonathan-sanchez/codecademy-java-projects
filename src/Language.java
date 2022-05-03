cd4bda6db85fc7fdda8432708eb690cfa2379538
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-05-02 21:12:43 -0500

    feat: added Language constructor alond with protected instances

---
+++ src/Language.java
@@ -0,0 +1,15 @@
+public class Language {
+
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
+    Language() {
+
+    }
+    
+    public static void main(String[] args) {
+
+    }
+}
