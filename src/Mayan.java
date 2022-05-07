591c8fd42b7f3275e8a82ba2bbb244c96774365f
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-05-07 10:18:09 -0500

    feat: added override method getInfo from Language.java

--- src/Mayan.java
+++ src/Mayan.java
@@ -5,5 +5,15 @@ public class Mayan extends Language {
         super(languageName, speakers, "Central America", "verb-object-subject");
 
     }
+
+    @Override
+    public void getInfo(){
+
+        System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + " people mainly in " + this.regionsSpoken + ".");
+
+        System.out.println("The language follows the word order: " + this.wordOrder);
+        
+        System.out.println("Fun facts: " + this.name + " is an ergative language.");
+    }
     
 }
