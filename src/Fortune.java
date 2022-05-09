328d0ca218e3b2a3ebe91c1c80a7ee40a1d358f4
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-05-08 21:38:43 -0500

    completed: Exercise3.java

--- src/Fortune.java
+++ src/Fortune.java
@@ -1,57 +1,57 @@
 public class Fortune {
-    
+	
 
-    public int generateRandom() {
+	public int generateRandom() {
 
-        return (int)(Math.random() * 10);
+		return (int)(Math.random() * 10);
 
-    }
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Fortune fortuneCookie = new Fortune();
-        int fortune = fortuneCookie.generateRandom();
+		Fortune fortuneCookie = new Fortune();
+		int fortune = fortuneCookie.generateRandom();
 
-        if (fortune == 0) {
+		if (fortune == 0) {
 
-            System.out.println("Flattery will go far tonight.");
+			System.out.println("Flattery will go far tonight.");
 
-        } else if (fortune == 1) {
+		} else if (fortune == 1) {
 
-            System.out.println("Dont behave with cold manners.");
+			System.out.println("Dont behave with cold manners.");
 
-        } else if (fortune == 2) {
+		} else if (fortune == 2) {
 
-            System.out.println("May you someday be carbon neutral.");
+			System.out.println("May you someday be carbon neutral.");
 
-        } else if (fortune == 3) {
+		} else if (fortune == 3) {
 
-            System.out.println("You have rice in your teeth.");
+			System.out.println("You have rice in your teeth.");
 
-        } else if (fortune == 4) {
+		} else if (fortune == 4) {
 
-            System.out.println("A conclusion is simply the place wheere you got tired of thinking.");
+			System.out.println("A conclusion is simply the place wheere you got tired of thinking.");
 
-        } else if (fortune == 5) {
+		} else if (fortune == 5) {
 
-            System.out.println("No snowflake feels responsible in an avalanche.");
+			System.out.println("No snowflake feels responsible in an avalanche.");
 
-        } else if (fortune == 6) {
+		} else if (fortune == 6) {
 
-            System.out.println("He who laughs last is laughing at you.");
+			System.out.println("He who laughs last is laughing at you.");
 
-        } else if (fortune == 7) {
+		} else if (fortune == 7) {
 
-            System.out.println("If you look back, you'll soon be going that way.");
+			System.out.println("If you look back, you'll soon be going that way.");
 
-        } else if (fortune == 8) {
+		} else if (fortune == 8) {
 
-            System.out.println("You will die alone and poorly dressed.");
+			System.out.println("You will die alone and poorly dressed.");
 
-        } else if (fortune == 9) {
+		} else if (fortune == 9) {
 
-            System.out.println("The fortune you seek is in another cookie.");
-        }
+			System.out.println("The fortune you seek is in another cookie.");
+		}
 
-    }
+	}
 }
--- src/twoDArrays/Exercise3.java
+++ src/twoDArrays/Exercise3.java
@@ -1,10 +1,24 @@
 package twoDArrays;
 
 public class Exercise3 {
-    
+	
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        
-    }
+		//Using the provided 2D array
+		int[][] intMatrix = {
+			{1, 1, 1, 1, 1},
+			{2, 4, 6, 8, 0},
+			{9, 8, 7, 6, 5}
+		};
+
+		// Access the integer at the first row and fourth column of intMatrix and store it in a variable called retrievedInt
+
+		int retrievedInt = intMatrix[0][3];
+		System.out.println(retrievedInt);
+
+		// Print 3 times the center value of intMatrix to the console. Make sure to access the correct element!
+
+		System.out.println(intMatrix[1][2] * 3);
+	}
 }
